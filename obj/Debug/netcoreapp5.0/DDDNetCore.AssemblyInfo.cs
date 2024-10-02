004077cb7e754a45c731717b76a84b1bb149b9b7
Beatriz Costa <1211588@isep.ipp.pt>
2024-10-02 09:25:38 +0100

    more changes

--- obj/Debug/netcoreapp5.0/DDDNetCore.AssemblyInfo.cs
+++ obj/Debug/netcoreapp5.0/DDDNetCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34149e1e5313570259b66844e3c211f401a07edc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+171b7bca6025a0dd9b946bd3734b2a2ab266f961")]
 [assembly: System.Reflection.AssemblyProductAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DDDNetCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
